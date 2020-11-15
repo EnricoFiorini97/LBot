c83e085aba2a39a7bd6aee47cfed4bd518cfb56d
EnricoFiorini97 <enricofiorini899@gmail.com>
2020-11-15 01:56:32 +0100

    Second commit

---
+++ src/main.py
@@ -0,0 +1,38 @@
+import discord
+from discord import File
+from discord.ext import commands
+from discord.ext.commands import Bot
+import os
+from pathlib import Path
+
+class DBot():
+    def __init__(self, prefix = "/"):
+        self.client = commands.Bot(command_prefix = prefix, intents=discord.Intents.all())
+        self.config_loader()
+
+    def config_loader(self):
+        token_file_path = r"" + str(Path(os.getcwd()).parent) + os.path.sep + "config_files" + os.path.sep + "token.txt"
+        
+        @self.client.command(description = "Load cog file")
+        async def load(ctx,extension):
+            self.client.load_extension(f"cogs.{extension}")
+
+        @self.client.command()
+        async def unload(ctx,extension):
+            self.client.unload_extension(f"cogs.{extension}")
+        
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py"):
+                self.client.load_extension(f"cogs.{filename[:-3]}")
+        
+        
+
+
+        self.client.run(open(token_file_path,"r").readline())
+
+
+def main():
+    DBot("$")
+
+if __name__ == "__main__":  main()
+
